8019f2e80d1278c9bca6416c2b9a728402b8ee4c
Yassine ZAIM <ZaimYassine>
2020-11-05 16:56:11 +0100

    La création du fichier TP version Spyder 05.11.2020 à 16h46

---
+++ TP1_spy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  5 15:01:21 2020
+
+@author: yassi
+"""
+
+#Librairies
+import pandas as pd
+import numpy as np 
+
+#Importation du fichier CSV
+df = pd.read_csv('input/googleplaystore.csv')
+
